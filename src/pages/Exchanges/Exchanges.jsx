d875f33631f926213c35cb909edab9f4bec10354
AsleyT <torrespalacioas@gmail.com>
2025-10-29 12:23:22 -0500

    Se añadió diseño a la página de Intercambios

--- src/pages/Exchanges/Exchanges.jsx
+++ src/pages/Exchanges/Exchanges.jsx
@@ -2,9 +2,13 @@ import {useState, useEffect} from "react";
 import "./Exchanges.css"
 
 const Exchanges = () => {
-    return(
-        <div>
-            Espacio para trabajar la página de intercambios
+    return( 
+    
+        <div className="container_main_exchanges">
+            <div className="container_exchange">
+                <h1 className="exchange_title">Encuentra tu próximo tesoro</h1>
+                <p className="exchange_info">Explora una variedad de artículos únicos y encuentra el intercambio perfecto para ti, dale una segunda vida a lo que ya no usas. Podrás utilizar tus <strong>SwapCoins</strong> para realizar intercambios con otros usuarios.</p>
+            </div>
         </div>
     )
 }
